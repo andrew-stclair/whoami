573da2d07b7b357c259cb473c7d758e5aefab437
Andrew St Clair <andrew_stclair95@hotmail.com>
2023-11-14 09:18:52 +0000

    Update

--- app/whoami.py
+++ app/whoami.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """Index function, just returns request headers"""
-    response = f"{request.remote_addr}\n"
+    ip = request.remote_addr
+    if "X-Forwarded-For" in request.headers:
+        ip = request.headers['X-Forwarded-For']
+    response = f"Ip: {ip}\n"
     for key in request.headers:
         response += f"{key[0]}: {key[1]}\n"
     result = Response(response)
